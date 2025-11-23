b321c84f24352c04406766ead59a3f202c53566e
dave-the-desk <dvriede@gmail.com>
2025-11-22 17:10:07 -0800

    Remove environment files with Firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  staging: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA0Y0F8zpD4I21cTRHtvJzsALncHhSpqQs',
+    authDomain: 'ai-fitness-f8ed4.firebaseapp.com',
+    databaseURL: 'https:/ai-fitness-f8ed4-default-rtdb.firebaseio.com/',
+    projectId: 'ai-fitness-f8ed4',
+    storageBucket: 'ai-fitness-f8ed4.firebasestorage.app',
+    messagingSenderId: '331636214459',
+    appId: '1:331636214459:web:5508ada3115c4fe86d779e',
+    measurementId: 'G-1YF7X3YMXV'
+  },
+  stripePublicKey: '67',
+  cloudFunctionsBaseUrl: '',
+  appVersion: '1.0',
+};
