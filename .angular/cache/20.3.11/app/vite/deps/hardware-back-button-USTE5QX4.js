0268b4d72624957be4e93478f39dfd103ac48863
dave-the-desk <dvriede@gmail.com>
2025-11-22 19:14:56 -0800

    removed app data temporarily to build

---
+++ .angular/cache/20.3.11/app/vite/deps/hardware-back-button-USTE5QX4.js
@@ -0,0 +1,17 @@
+import {
+  MENU_BACK_BUTTON_PRIORITY,
+  OVERLAY_BACK_BUTTON_PRIORITY,
+  blockHardwareBackButton,
+  shouldUseCloseWatcher,
+  startHardwareBackButton
+} from "./chunk-XONSWJIQ.js";
+import "./chunk-QEE7QVES.js";
+import "./chunk-2H3NLAAY.js";
+import "./chunk-4VWZEZJW.js";
+export {
+  MENU_BACK_BUTTON_PRIORITY,
+  OVERLAY_BACK_BUTTON_PRIORITY,
+  blockHardwareBackButton,
+  shouldUseCloseWatcher,
+  startHardwareBackButton
+};
